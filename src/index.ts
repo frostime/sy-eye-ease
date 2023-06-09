271f6d50fcee4bb253da9221cc96826aede1aed4
frostime <frostime@foxmail.com>
2023-06-09 16:10:29 +0800

    添加 debounce 函数

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,39 @@ const CHECK_REST_INTERVAL = 5 * 60; // 5 minutes, 检查如果电脑休眠了，
 let UnMaskScreenEvent: EventListener;
 let WsEvent: EventListener;
 
+function debounce<T extends (...args: any[]) => void>(
+    callback: T,
+    wait: number,
+    immediate = false,
+) {
+    // This is a number in the browser and an object in Node.js,
+    // so we'll use the ReturnType utility to cover both cases.
+    let timeout: ReturnType<typeof setTimeout> | null;
+
+    return function <U>(this: U, ...args: Parameters<typeof callback>) {
+        const context = this;
+        const later = () => {
+            timeout = null;
+
+            if (!immediate) {
+                callback.apply(context, args);
+            }
+        };
+        const callNow = immediate && !timeout;
+
+        if (typeof timeout === "number") {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(later, wait);
+
+        if (callNow) {
+            callback.apply(context, args);
+        }
+    };
+}
+
+
 export default class PluginSample extends Plugin {
 
     maskDiv: HTMLDivElement;
